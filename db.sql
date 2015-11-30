6ddac8df8bb9ebc5fb5332f737666fd0d7d51d2e
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-30 12:55:11 +0100

    no more needed (wp cli db)

--- db.sql
+++
@@ -1,5 +0,0 @@
-create database DB_NAME;
-use DB_NAME;
-alter database DB_NAME CHARACTER SET utf8 COLLATE utf8_polish_ci;
-create user DB_USER identified by 'DB_PASSWORD';
-grant all on DB_NAME to DB_USER;
