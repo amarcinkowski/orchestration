f3d1c637dab57e4befbdbf21a6218017ff8e3d4b
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-30 09:02:10 +0100

    impr

--- gs.sh
+++ gs.sh
@@ -4,6 +4,6 @@ for D in repo/*;
 do
   if [ -d "${D}" ]; then
 	echo "== GIT STATUS ${D}"
-	git -C "${D}" status
+	git -C "${D}" status | grep -v "On branch master" | grep -v "Your branch is up-to-date" | grep -v "nothing to commit"
   fi
 done
